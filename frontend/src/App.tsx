67a500f10d51a6eb65668563368743e86a4bb36f
Pablo <pablomelo.inf@gmail.com>
2022-11-18 00:21:14 -0300

    first component react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+    </>
   )
 
 }
